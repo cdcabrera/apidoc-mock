e9b2861ddc419f9051da2c259b5c0f648797cca9
CD Cabrera <cabrera.code@gmail.com>
2019-11-24 22:07:58 -0500

    fix(build): expand integration like tests for apidoc output

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -19,6 +19,8 @@ describe('ApiDocMock', () => {
   });
 
   it('should create a predictable docs output', () => {
+    const outputDir = './.docs';
+
     const { dir } = tempFile(`/**
      * @api {get} /hello/world/
      * @apiSuccessExample {json} Success-Response:
@@ -33,13 +35,28 @@ describe('ApiDocMock', () => {
      *     }
      */`);
 
-    const [helloWorld] = setupDocs(dir, './.docs');
+    const [helloWorld] = setupDocs(dir, outputDir);
+    const fileOutput = execSync(`find ${outputDir} -type f -print0 | xargs -0`);
+
+    const cleanUpFileOutput = fileOutput
+      .toString()
+      .replace(/\s+|\n+|\r+/g, '')
+      .replace(new RegExp(`${outputDir}`, 'gi'), `~${outputDir}`)
+      .replace(new RegExp(`~${outputDir}/.DS_Store`, 'gi'), '')
+      .replace(/\.([a-z0-9]+)\./gi, '*')
+      .split('~')
+      .sort();
+
+    expect(cleanUpFileOutput).toMatchSnapshot('specific file output');
 
     expect({
       type: helloWorld.type,
+      title: helloWorld.title,
       url: helloWorld.url,
       success: JSON.stringify(helloWorld.success),
-      error: JSON.stringify(helloWorld.error)
+      error: JSON.stringify(helloWorld.error),
+      version: helloWorld.version,
+      name: helloWorld.name
     }).toMatchSnapshot('setupDocs');
   });
 
